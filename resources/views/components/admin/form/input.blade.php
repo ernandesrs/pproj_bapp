ceb411799aa16da803888043cd4b10f34137d7d2
Ernandes R Souza <ernandesrsouza@gmail.com>
2024-05-10 14:12:00 -0400

    input text/number/password/select in one file

--- resources/views/components/admin/form/input.blade.php
+++ resources/views/components/admin/form/input.blade.php
@@ -7,7 +7,9 @@
     $id = $type . '_input_' . uniqid();
     $styles =
         'shadow border ' .
-        ($errors->has($attributes->get('wire:model')) ? 'border-admin-danger text-admin-danger' : 'border-admin-light text-admin-font-light') .
+        ($errors->has($attributes->get('wire:model'))
+            ? 'border-admin-danger text-admin-danger'
+            : 'border-admin-light text-admin-font-light') .
         ' px-4 py-2 w-full focus:outline-none focus:border-admin-font-light disabled:bg-admin-light disabled:bg-opacity-60 readonly:bg-admin-light readonly:bg-opacity-60';
 
     $attributes = $attributes->merge([
@@ -18,9 +20,50 @@
     ]);
 @endphp
 
-@if (in_array($type, ['text', 'password', 'number']))
-    <x-admin.form.inputs.input input-styles="{{ $styles }}" {{ $attributes }} />
-@elseif ($type == 'select')
-    <x-admin.form.inputs.select input-styles="{{ $styles }}" {{ $attributes }} />
-@elseif ($type == 'textarea')
-@endif
+<div {{ $attributes->only(['class'])->merge(['class' => 'relative']) }}>
+    @if ($attributes->has('label'))
+        <label class="text-admin-font-light mb-1 block" for="{{ $attributes->get('id') }}">
+            {{ $attributes->get('label') }}
+        </label>
+    @endif
+    <div
+        x-data="{
+            ...{{ json_encode(['originalType' => $attributes->get('type'), 'type' => $attributes->get('type')]) }},
+
+            togglePasswordInputVisibility() {
+                if (this.originalType != 'password') {
+                    return;
+                }
+
+                this.type = this.type == 'password' ? 'text' : 'password';
+            }
+        }"
+        class="relative">
+
+        @if (in_array($type, ['text', 'password', 'number']))
+            <input {{ $attributes->except(['class', 'type']) }}
+                :type="type"
+                class="{{ $styles }}" />
+
+            @if ($attributes->get('type') == 'password')
+                <button
+                    x-on:click.prevent="togglePasswordInputVisibility"
+                    class="absolute h-full top-0 right-0 px-3 border border-transparent">
+                    <x-admin.icon x-show="type == 'password'" name="eye-fill" />
+                    <x-admin.icon x-show="type == 'text'" name="eye-slash-fill" />
+                </button>
+            @endif
+        @elseif ($type == 'select')
+            <select {{ $attributes->except(['class', 'type', 'options']) }}
+                class="{{ $styles }}">
+                @foreach ($attributes->get('options') as $option)
+                    <option value="{{ $option['value'] }}">{{ $option['label'] }}</option>
+                @endforeach
+            </select>
+        @endif
+    </div>
+    @if ($errors->has($attributes->get('wire:model')))
+        <x-admin.form.feedback color="danger"
+            text="{{ implode('.', $errors->get($attributes->get('wire:model'))) }}" />
+    @endif
+</div>
